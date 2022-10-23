810165dff6790cf5e79c737149d698980b101ce2
Andrew <72750362+andrewc-moj@users.noreply.github.com>
2022-10-23 15:16:29 +0100

    Remove __main__ code

--- code/functions.py
+++ code/functions.py
@@ -184,31 +184,3 @@ class Simulations():
 
 
 
-if __name__ == "__main__":
-
-    # sim = Simulations(2, 1)
-    # sim.run_simulations()
-
-    
-    # # Start small with 2 players
-    num_players = 2
-
-    # Simulations = 1
-    num_simulations = 10000
-
-    # Initialise simulation
-    sim = Simulations(num_players, num_simulations)
-
-    # Run simulations
-    sim.run_simulations()
-
-    # Print out winning records
-    sim.winner_records
-
-    # sim.winner_records
-
-    # # Init Game
-    # game = Game(num_players)
-
-    # # Play a round over all players
-    # game.play_game()
